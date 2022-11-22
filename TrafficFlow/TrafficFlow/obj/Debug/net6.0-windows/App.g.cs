dbdb08b16d5f23d60f55d555ef86a38477c8c619
hoizopfe <thomas.holzapfel@sz-ybbs.ac.at>
2022-11-22 09:49:25 +0100

    RadioButton Fix

--- TrafficFlow/TrafficFlow/obj/Debug/net6.0-windows/App.g.cs
+++ TrafficFlow/TrafficFlow/obj/Debug/net6.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace TrafficFlow {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace TrafficFlow {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             TrafficFlow.App app = new TrafficFlow.App();
             app.InitializeComponent();
